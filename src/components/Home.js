493d9607b984e1f4ef3ede257d452b724b4450ea
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-05-07 14:36:27 +0100

    starting up

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>repository name</h2>
+      <p>Language </p>
+      <p>Language: JavaScript</p>
+      <p>Visibility: Public</p>
+    </div>
+  );
+};
+
+export default Home;
