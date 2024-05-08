7b2a72461bf299cb73206e040c704a526cddab5a
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-05-08 13:12:31 +0100

    coming along

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,39 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-const Home = () => {
+function Home() {
+  const [user, setUser] = useState([]);
+
+  const fetchRepos = () => {
+    fetch(`https://api.github.com/users/codekazuki/repos`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchRepos();
+  }, []);
+
+  const userElements = user.map((userElement) => {
+    return (
+      <div className='repo-card' key={userElement.id}>
+        <h2 className='repo-name'>{userElement.name}</h2>
+        <p className='language'>
+          Langauge:{" "}
+          {userElement.language === null ? "none" : userElement.language}
+        </p>
+        <p className='date'>Start date & time: {userElement.created_at}</p>
+        <p className='visibility'>Visibility: {userElement.visibility}</p>
+      </div>
+    );
+  });
+
   return (
-    <div>
-      <h2>repository name</h2>
-      <p>Language </p>
-      <p>Language: JavaScript</p>
-      <p>Visibility: Public</p>
-    </div>
+    <>
+      <section className='repo-container'>{userElements}</section>
+    </>
   );
-};
+}
 
 export default Home;
